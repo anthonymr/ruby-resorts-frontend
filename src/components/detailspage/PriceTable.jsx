bdddf095edbe133266cb2be154dcd8e6a0cd175f
Ghulam Subbhani <gsmalik030@gmail.com>
2023-06-12 07:41:57 +0200

    Added price table component

---
+++ src/components/detailspage/PriceTable.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import {detailsTable} from '../styles/styles'
+const StyledTableCell = styled(TableCell)(() => ({
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 16,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    ' td, th': {
+      border: 0,
+    },
+  }));
+  
+  function createData(
+    name: string,
+    price: number
+  ) {
+    return { name, price };
+  }
+  
+  const rows = [
+    createData('Price', 100),
+    createData('Transaction fee', 237),
+    createData('Total amount', 262),
+    createData('Duraiton', 305),
+  ];
+
+const PriceTable = () => {
+    return (
+        <TableContainer component={Paper}>
+          <Table sx={detailsTable}>
+            <TableBody>
+              {rows.map((row) => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.name}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{row.price}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      );
+    }
+
+export default PriceTable
