b9644c810e9398186e96f852737f06bd06de0082
Ghulam Subbhani <gsmalik030@gmail.com>
2023-06-15 16:01:25 +0200

    Api integration deleteRoom

--- src/components/deleteRoom/DeleteRoom.jsx
+++ src/components/deleteRoom/DeleteRoom.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useGetRoomsListQuery, useDeleteRoomQuery } from '../../services/apiService';
+import { getRoomsList } from '../../redux/mainPage/roomsSlice';
 import {
   Typography,
+  Container,
   Button,
   Table,
   TableBody,
@@ -10,95 +13,55 @@ import {
   TableHead,
   TableRow,
   Paper,
-  Box,
 } from '@mui/material';
-import { useDeleteRoomMutation } from '../../services/apiService';
-import { deleteFromList } from '../../redux/mainPage/roomsSlice';
 
 const DeleteRoom = () => {
-  const { rooms } = useSelector((state) => state.rooms);
-  const [roomId, setRoomId] = useState(0);
-  const [deleteRoom, response] = useDeleteRoomMutation('delteRoom');
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (response.isSuccess) dispatch(deleteFromList(roomId));
-  }, [dispatch, response, roomId]);
+  const { data: rooms, refetch } = useGetRoomsListQuery();
+  const deleteRoom = useDeleteRoomQuery();
 
-  const handleDelete = (roomId) => {
-    setRoomId(roomId);
-    deleteRoom(roomId);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (rooms) {
+      dispatch(getRoomsList(rooms));
+    }
+  }, [rooms, dispatch]);
+
+  const { rooms: roomList } = useSelector((state) => state.rooms);
+
+  const handleDelete = async (roomId) => {
+    try {
+      await deleteRoom.query(roomId);
+      refetch();
+    } catch (error) {
+      console.error('Error deleting room:', error);
+    }
   };
 
   return (
-    <Box
-      sx={{
-        width: {
-          xs: '100%',
-          sm: '80%',
-          md: '85%',
-          lg: '88%',
-        },
-        minHeight: '100vh',
-        margin: '0 auto',
-        padding: { xs: '25% 0 0', sm: '0' },
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: { xs: 'start', sm: 'center' },
-        alignItems: 'center',
-        position: 'relative',
-      }}
-    >
-      <Typography
-        variant="h4"
-        fontWeight={900}
-        color="black"
-        letterSpacing="3px"
-        sx={{ margin: '2rem' }}
-      >
-        DELETE A SUITE
+    <Container>
+      <Typography variant="h1" component="h1" sx={{ textAlign: 'center', marginBottom: '20px' }}>
+        Ruby Resorts
       </Typography>
-      <TableContainer
-        component={Paper}
-        sx={{ margin: '0 auto', maxWidth: 600 }}
-      >
+      <Typography variant="h6" component="h6" sx={{ textAlign: 'center', marginBottom: '20px' }}>
+        Delete the Rooms
+      </Typography>
+      <TableContainer component={Paper} sx={{ margin: '0 auto', maxWidth: 600 }}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell
-                sx={{
-                  fontSize: '1.2rem',
-                  fontWeight: 700,
-                }}
-                align="center"
-              >
-                Suite
-              </TableCell>
-              <TableCell
-                align="center"
-                sx={{ fontSize: '1.2rem', fontWeight: 700 }}
-              >
-                Action
-              </TableCell>
+              <TableCell>Room Name</TableCell>
+              <TableCell>Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rooms.map((room) => (
+            {roomList.map((room) => (
               <TableRow key={room.id}>
-                <TableCell
-                  align="center"
-                  sx={{
-                    fontSize: '1rem',
-                    fontWeight: 700,
-                    color: 'text.third',
-                  }}
-                >
-                  {room.name}
-                </TableCell>
-                <TableCell align="center">
+                <TableCell>{room.name}</TableCell>
+                <TableCell>
                   <Button
                     variant="contained"
                     color="secondary"
-                    sx={{ color: '#ffffff', fontWeight: 700 }}
                     onClick={() => handleDelete(room.id)}
                   >
                     Delete
@@ -109,28 +72,7 @@ const DeleteRoom = () => {
           </TableBody>
         </Table>
       </TableContainer>
-      {response.isSuccess && (
-        <Typography
-          variant="h6"
-          fontWeight={700}
-          color="text.fourth"
-          sx={{ margin: '1rem' }}
-        >
-          Suite deleted successfully!!
-        </Typography>
-      )}
-
-      {response.isError && (
-        <Typography
-          variant="h6"
-          fontWeight={700}
-          color="text.error"
-          sx={{ margin: '1rem' }}
-        >
-          Error deleting the Suite. Try again later
-        </Typography>
-      )}
-    </Box>
+    </Container>
   );
 };
 
