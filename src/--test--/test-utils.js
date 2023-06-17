9d1d137284b608badca4a4a1e45cc6a1b7ff9737
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-06-17 15:38:35 +0200

    Added prop validaion

--- src/--test--/test-utils.js
+++ src/--test--/test-utils.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { render as rtlRender } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 
-// Custom render method that includes the Redux Provider
 const render = (ui, options) => {
   const Wrapper = ({ children }) => <Provider store={store}>{children}</Provider>;
+  Wrapper.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
   return rtlRender(ui, { wrapper: Wrapper, ...options });
 };
 
